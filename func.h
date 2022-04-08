35c2eb2fb83efa6079e3c7a5fbdaaf3c600c3438
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2022-04-07 22:10:11 -0400

    added rooms array

--- func.h
+++ func.h
@@ -2,6 +2,7 @@ void menu();
 void menu(){
     int roomsize;
     int loop = 0;
+    int room[10]={1,1,1,2,2,2,3,3,3,1};
     cout << "Seclect the number of beds"<<endl<<"*note*- We only have rooms of max 3 beds"<<endl;
     cout << "Enter 1 = 1 bedroom"<<endl<< "Enter 2 = 2 bedroom"<<endl<< "Enter 1 = 1 bedroom"<<endl;
     cout << "Input: ";
@@ -10,16 +11,28 @@ void menu(){
     if (roomsize == 1){
         loop =1;
         cout<<"showing aviable rooms with 1 bedroom"<<endl;
+         for (int i = 0; i < 10; i++)
+        {
+            cout << "Room "<<i+1<<" -Number of beds: "<<room[i]<<endl;
+        }
         break;
     }
     else if(roomsize == 2){
         loop =1;
-        cout<<"showing aviable rooms with 2 bedroom"<<endl;   
+        cout<<"showing aviable rooms with 2 bedroom"<<endl; 
+        for (int i = 0; i < 10; i++)
+        {
+            cout << "Room "<<i+1<<" -Number of beds: "<<room[i]<<endl;
+        }  
         break;
     }
     else if (roomsize == 3){
         loop =1;
         cout<<"showing aviable rooms with 3 bedroom"<<endl;
+        for (int i = 0; i < 10; i++)
+        {
+            cout << "Room "<<i+1<<" -Number of beds: "<<room[i]<<endl;
+        }
         break;
     }
     else {
