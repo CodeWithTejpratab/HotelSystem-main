37792c0de9048dcaed388ca51abc11006d0457f5
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2022-04-08 00:53:38 -0400

    removed menu func

--- program.cpp
+++ program.cpp
@@ -1,7 +1,52 @@
 void program(){
     cout<<endl;
     cout<<"Welcome to Booking.com"<<endl;
-    menu();
-
+    int roomsize;
+    int loop = 0;
+    int const room[10]={1,1,1,2,2,2,3,3,3,1};
+    cout << "Seclect the number of beds"<<endl<<"*note*- We only have rooms of max 3 beds"<<endl;
+    cout << "Enter 1 = 1 bedroom"<<endl<< "Enter 2 = 2 bedroom"<<endl<< "Enter 1 = 1 bedroom"<<endl;
+    cout << "Input: ";
+    cin >> roomsize;
+    while (loop!=1){
+    if (roomsize == 1){
+        loop =1;
+        cout<<endl<<"Showing aviable rooms with 1 bedroom"<<endl;
+         for (int i = 0; i < 10; i++)
+        {
+            if(*(room+i)==1){
+            cout << "Room "<<i+1<<" -Number of beds: "<<room[i]<<endl;
+            }
+        }
+        break;
+    }
+    else if(roomsize == 2){
+        loop =1;
+        cout<<endl<<"Showing aviable rooms with 2 bedroom"<<endl; 
+        for (int i = 0; i < 10; i++)
+        {
+            if(*(room+i)==2){
+            cout << "Room "<<i+1<<" -Number of beds: "<<room[i]<<endl;
+            }
+        }  
+        break;
+    }
+    else if (roomsize == 3){
+        loop =1;
+        cout<<endl<<"Showing aviable rooms with 3 bedroom"<<endl;
+        for (int i = 0; i < 10; i++)
+        {
+            if(*(room+i)==3){
+            cout << "Room "<<i+1<<" -Number of beds: "<<room[i]<<endl;
+            }
+        }
+        break;
+    }
+    else {
+        cout << "Unavailable, Try again"<<endl;
+        cout << "Input: ";
+        cin >> roomsize;
+    }
+}
     cout<<endl;
 }
