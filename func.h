5c99904e7bf23f68bb0007c5dcc3f1e39e42f58e
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2022-04-08 00:50:51 -0400

    sorted room & made (const)

--- func.h
+++ func.h
@@ -2,7 +2,7 @@ void menu();
 void menu(){
     int roomsize;
     int loop = 0;
-    int room[10]={1,1,1,2,2,2,3,3,3,1};
+    int const room[10]={1,1,1,2,2,2,3,3,3,1};
     cout << "Seclect the number of beds"<<endl<<"*note*- We only have rooms of max 3 beds"<<endl;
     cout << "Enter 1 = 1 bedroom"<<endl<< "Enter 2 = 2 bedroom"<<endl<< "Enter 1 = 1 bedroom"<<endl;
     cout << "Input: ";
@@ -10,28 +10,34 @@ void menu(){
     while (loop!=1){
     if (roomsize == 1){
         loop =1;
-        cout<<"showing aviable rooms with 1 bedroom"<<endl;
+        cout<<endl<<"Showing aviable rooms with 1 bedroom"<<endl;
          for (int i = 0; i < 10; i++)
         {
+            if(*(room+i)==1){
             cout << "Room "<<i+1<<" -Number of beds: "<<room[i]<<endl;
+            }
         }
         break;
     }
     else if(roomsize == 2){
         loop =1;
-        cout<<"showing aviable rooms with 2 bedroom"<<endl; 
+        cout<<endl<<"Showing aviable rooms with 2 bedroom"<<endl; 
         for (int i = 0; i < 10; i++)
         {
+            if(*(room+i)==2){
             cout << "Room "<<i+1<<" -Number of beds: "<<room[i]<<endl;
+            }
         }  
         break;
     }
     else if (roomsize == 3){
         loop =1;
-        cout<<"showing aviable rooms with 3 bedroom"<<endl;
+        cout<<endl<<"Showing aviable rooms with 3 bedroom"<<endl;
         for (int i = 0; i < 10; i++)
         {
+            if(*(room+i)==3){
             cout << "Room "<<i+1<<" -Number of beds: "<<room[i]<<endl;
+            }
         }
         break;
     }
