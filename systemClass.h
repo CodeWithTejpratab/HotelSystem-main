a649f16c894ccda186a6ef0e603c552d938be1bb
Rahul Ramjeawon <96823012+CodeWithTejpratab@users.noreply.github.com>
2024-02-14 20:41:44 -0500

    fixed class inheritance system

--- systemClass.h
+++ systemClass.h
@@ -1,57 +1,56 @@
 #include "wrapper.hpp"
 #define CLASSES
 
-class hotel_system{
-    int* room;
-    double price1, price2, price3;
+class Hotel_system: public Service{
+    bool room[10];
+    int roomSize[10]; // 1 bed, 2 bed
+    double price1, price2;
+
 public:
-    hotel_system();
+    Hotel_system();
     void printroom();
-    ~hotel_system();
-    int getroom(int);
+    //~hotel_system(); //No destructors needed 
+    bool getroom(int);
     double getprice1();
     double getprice2();
-    double getprice3();
+    friend class Client;
 };
 
-class client{
+class Client{
     std::string firstName;
     std::string lastName;
-    long int ID;
-    long int Number;
+    int roomNumber;
     int nights;
 
 public:
-    //friend class service;
-    client();
+    friend class Hotel_system;
+    Client();
     void setfirstName(std::string);
     void setlastName(std::string);
-    void setID(long int);
-    void setNumber(long int);
+    void setRoomNumber(long int);
     void setnights(int);
     std::string getfirstname();
     std::string getlastname();
-    int getID();
-    int getNumber();
+    int getRoomNumber();
     int getnights();
 };
 
-class service : public hotel_system, public client{
-    bool roomService;
-    bool poolService;
-    bool publicService;
-    double priceRoom, pricePool, pricePublic;
+class Service {
+    bool roomService[10];
+    bool poolService[10];
+    bool publicService[10];
+    double priceRoomService, pricePool, pricePublicService;
 
 public:
     //friend class client;
-    service();
-    bool getroomservice();
-    bool getpoolservice();
-    bool getpublicservice();
-    double getpriceRoom();
-    double getpricePool();
-    double getpricePublic();
-    void setroomService(bool);
-    void setpoolService(bool);
-    void setpublicService(bool);
+    Service();
+    bool getRoomService(int);
+    bool getPoolService(int);
+    bool getPublicService(int);
+    double getPriceRoom();
+    double getPricePool();
+    double getPricePublic();
+    void setRoomService(int);
+    void setPoolService(int);
+    void setPublicService(int);
 };
